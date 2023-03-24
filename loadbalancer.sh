40e5edcf2550625b9006a1a291c73c50ca48a3a3
Jeff <j.koenigs@ecole-ipssi.net>
2023-03-24 13:49:18 +0000

    job

--- loadbalancer.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-EXPORT DEBIAN_FRONTEND=noninteractive
-apt update
-apt install -y haproxy
-
--- webserver.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-EXPORT DEBIAN_FRONTEND=noninteractive
-apt update
-apt install -y apache2
